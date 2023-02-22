4be67660f280e479ccaef37aba095fe584f47515
bapatist <nikhilabhaybapat@gmail.com>
2023-02-22 11:29:38 +0100

    Binner_to_post_process

--- analyse_dfs.py
+++ analyse_dfs.py
@@ -8,7 +8,7 @@ csv_files = glob.glob("./CSVs/cus*.csv")
 fig, axs = plt.subplots(3,1)
 dfs = []
 for ind, file in enumerate(csv_files):
-    df   = pd.read_csv(file)[20:]
+    df   = pd.read_csv(file)[2:]
     dfs.append(df)
     axs[0].hist(df['Free_Energy'], bins=50, label=ind)
     axs[1].hist(df['OH'], label=ind)
@@ -17,6 +17,12 @@ axs[1].legend()
 #%%
 df_joint = pd.concat(dfs)
 fig, axs = plt.subplots()
-axs.plot(df_joint['OO'], df_joint['Free_Energy'], ls='None', marker='o')
+x_ax = df_joint['OO']
+y_ax = df_joint['Free_Energy']
+# axs.plot(x_ax, y_ax, ls='None', marker='o')
+hb = axs.hexbin(x_ax, y_ax, gridsize=20, mincnt=1, bins = 'log', cmap='GnBu')
+
+bin_means, bin_edges, binnumber = stats.binned_statistic(x_ax, y_ax, statistic='mean', bins=2+x_ax.max()-x_ax.min(), range=(x_ax.min()-1,x_ax.max()+1))
+axs.hlines(bin_means, bin_edges[:-1]-0.5, bin_edges[1:]-0.5, colors='k', lw=4,label='$Binned$ $Average$')
 
 # %%
